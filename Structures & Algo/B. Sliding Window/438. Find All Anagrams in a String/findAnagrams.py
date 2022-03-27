36335ce91aeea4607566d49a26bba9e3efa8abd1
jessieharada6 <jessie.harada@yahoo.com>
2022-03-27 20:03:57 +1100

    sliding window

---
+++ Structures & Algo/B. Sliding Window/438. Find All Anagrams in a String/findAnagrams.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        need = {}
+        have = {}
+        for c in p:
+            need[c] = need.get(c, 0) + 1
+
+        ans = []
+        l = r = 0
+        valid = 0
+        while r < len(s):
+            c = s[r]
+            r += 1        
+            if c in need:
+                have[c] = have.get(c, 0) + 1
+                if need[c] == have[c]:
+                    valid += 1
+            
+            while r - l == len(p):
+                if valid == len(need):
+                    ans.append(l)
+                    
+                d = s[l]
+                l += 1
+                if d in need:
+                    if need[d] == have[d]:
+                        valid -= 1
+                    have[d] -= 1
+        
+        return ans
+                    
+                    
+        
