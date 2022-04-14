c6fac6443bb39f6a5e10a7cf2194bddfca1e2c90
jessieharada6 <jessie.harada@yahoo.com>
2022-04-14 22:51:52 +1000

    reverse linked list

---
+++ Structures & Algo/A. Array & List/6. LinkedList/Recursion/206. Reverse Linked List/reverseList.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = None
+        while head:
+            next = head.next
+            head.next = p
+            
+            p = head
+            head = next
+        
+        return p
