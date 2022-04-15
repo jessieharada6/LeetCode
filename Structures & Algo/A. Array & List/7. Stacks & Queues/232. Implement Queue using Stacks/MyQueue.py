e3a9f7f119b70fa8ff646785898c23f42395ee40
jessieharada6 <jessie.harada@yahoo.com>
2022-04-16 08:18:11 +1000

    stack, queue

---
+++ Structures & Algo/A. Array & List/7. Stacks & Queues/232. Implement Queue using Stacks/MyQueue.py
@@ -0,0 +1,26 @@
+class MyQueue:
+
+    def __init__(self):
+        self.q = []
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        return self.q.pop(0)
+
+    def peek(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+# for the langs that have stack, 
+# use 2 stacks for peek (when s1 is empty, push the element from s2 to s1 so the order is right), then use peek() for pop()
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
