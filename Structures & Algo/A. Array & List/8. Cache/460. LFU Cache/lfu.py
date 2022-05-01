419235a638157c2bb3a1c194c021894faf5d0d31
jessieharada6 <jessie.harada@yahoo.com>
2022-05-02 09:29:26 +1000

    lfu

--- Structures & Algo/A. Array & List/8. Cache/460. LFU Cache/lfu.py
+++ Structures & Algo/A. Array & List/8. Cache/460. LFU Cache/lfu.py
@@ -2,6 +2,58 @@
 
 from collections import OrderedDict
 from collections import defaultdict
+
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = defaultdict(OrderedDict)
+        self.freq = defaultdict(int)
+        self.min = 1
+        self.size = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.freq:
+            # get freq and val, remove current val
+            freq = self.freq[key]
+            val = self.cache[freq][key]
+            del self.cache[freq][key]
+            
+            # update frequency and cache
+            self.freq[key] += 1
+            self.cache[freq + 1][key] = val
+            
+            # update min
+            if self.min == freq and not self.cache[freq]:
+                self.min += 1
+            return val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.freq:
+            # update value
+            self.get(key)
+            self.cache[self.freq[key]][key] = value
+        else:
+            self.size -= 1
+            
+            self.freq[key] = 1
+            self.cache[1][key] = value
+             
+            if self.size < 0:           # no need to increment self.size, as we will never remove any elements 
+                self.size = 0
+                # remove first item with min freq
+                k, v = self.cache[self.min].popitem(last=False)
+                del self.freq[k]
+                
+            self.min = 1
+        
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
 class LFUCache:
 
     def __init__(self, capacity: int):
