a5aea01f4ffa88f32749e761eaa5922814d2f37a
jessieharada6 <jessie.harada@yahoo.com>
2022-05-07 23:35:22 +1000

    trees

--- Structures & Algo/B. Tree/DP/95. Unique Binary Search Trees II/generateTrees.py
+++ Structures & Algo/B. Tree/DP/95. Unique Binary Search Trees II/generateTrees.py
@@ -4,23 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        def dfs(l, r):
+        def build(l, r):
             trees = []
             if l > r:
-                trees.append(None)
-            
-            for v in range(l, r + 1):
-                left = dfs(l, v - 1)
-                right = dfs(v + 1, r)
+                trees.append(None)                  # when 1 > 0, 1 as a root has no children
+                                                    # when l == r, it is a leaf node   
+            for n in range(l, r + 1):
+                                                    # left and right are unique as we only traverse each range once
+                left = build(l, n - 1)  
+                right = build(n + 1, r)
                 
-                for _l in left:
+                for _l in left:                     # left and right are presented as arrays
                     for _r in right:
-                        root = TreeNode(v)
+                        root = TreeNode(n)          # build root based on different combo of left and right
                         root.left = _l
                         root.right = _r
-                        trees.append(root)
+                        trees.append(root)          # append the unique tree
             return trees
-    
-        return dfs(1, n)
+        
+        return build(1, n)
