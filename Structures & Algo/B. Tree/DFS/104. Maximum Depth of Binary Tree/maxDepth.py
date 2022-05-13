593440e5e19f4b392a13d015c51b2ca5d8f3635a
jessieharada6 <jessie.harada@yahoo.com>
2022-05-13 21:16:18 +1000

    trees

--- Structures & Algo/B. Tree/DFS/104. Maximum Depth of Binary Tree/maxDepth.py
+++ Structures & Algo/B. Tree/DFS/104. Maximum Depth of Binary Tree/maxDepth.py
@@ -4,6 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        
+        return max(l, r) + 1
+        
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         def maxLevel(root):
