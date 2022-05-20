4a44d52e10494ca3ed01490978b6ac7c9f2592c3
jessieharada6 <jessie.harada@yahoo.com>
2022-05-20 20:57:40 +1000

    directed acyclic graph (DAG) traversal

---
+++ Structures & Algo/C. Graph/797. All Paths From Source to Target/allPathsSourceTarget.py
@@ -0,0 +1,42 @@
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        paths = []
+        path = []
+        
+        def traverse(node):
+            path.append(node)
+            
+            if node == len(graph) - 1:
+                paths.append(path[:])               # path shallow copy - path[:]
+                return
+            
+            for n in graph[node]:
+                traverse(n)
+                path.pop()
+                
+        
+        traverse(0)
+        return paths
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        paths = []
+        path = [0]
+        
+        def traverse(node):
+            # print("node", node)   # it will have the root value
+            if node == len(graph) - 1:
+                paths.append(path[:])
+                return
+            
+            for n in graph[node]:   # in the for loop, it won't have the root value
+                path.append(n)
+                # print("before n", n)
+                traverse(n)
+                # print("after n", n)
+                path.pop()
+                
+        
+        traverse(0)
+        return paths
