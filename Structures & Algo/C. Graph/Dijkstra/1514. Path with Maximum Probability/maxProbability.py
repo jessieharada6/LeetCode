9ad96c701df36f9d8f978af0b3ecb9842f36952e
jessieharada6 <jessie.harada@yahoo.com>
2022-06-03 21:28:50 +1000

    dijskstra

---
+++ Structures & Algo/C. Graph/Dijkstra/1514. Path with Maximum Probability/maxProbability.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        graph = defaultdict(list)
+        for ((e1, e2), cost) in zip(edges, succProb): # nodes to each other
+            graph[e1].append((e2, cost))
+            graph[e2].append((e1, cost))
+        # print(graph)
+        
+        heap = []
+        heapq.heappush(heap, (0, start))
+        
+        dist = [-math.inf for _ in range(n)]                # getting max so fill in -math.inf
+        dist[start] = 1                                     # note: the possibility from start to start is 1
+        
+        while heap:
+            c, cur = heapq.heappop(heap)
+            
+            for nei, d in graph[cur]:
+                # print(cur, nei, d)
+                if dist[cur] * d > dist[nei]:               # get max 
+                    dist[nei] = dist[cur] * d
+                    heapq.heappush(heap, (-dist[nei], nei)) # turn min heap to max heap
+        # print(dist)
+        return dist[end] if dist[end] != -math.inf else 0        
--- Structures & Algo/C. Graph/Dijkstra/743. Network Delay Time/networkDelayTime.py
+++ Structures & Algo/C. Graph/Dijkstra/743. Network Delay Time/networkDelayTime.py
@@ -19,15 +19,42 @@ class Solution:
         
         while heap:
             d, cur = heapq.heappop(heap)
-            if d > dist[cur]:  # can't get to another node
+            if d > dist[cur]:  
                 continue
             
             for nei, cost in graph[cur]:
                 if dist[cur] + cost < dist[nei]:
                     dist[nei] = dist[cur] + cost
-                    heapq.heappush(heap, (dist[nei], nei))
+                    heapq.heappush(heap, (dist[nei], nei))              # add the updated distance with the node to heap
         
         # print(dist)
         # min time to go to all nodes = max time to reach a node
         res = max(dist)
         return res if res != INF else -1
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        for edge in times:
+            graph[edge[0] - 1].append((edge[1] - 1, edge[2]))
+        
+        dist = [math.inf for _ in range(n)]
+        dist[k - 1] = 0
+        heap = []
+        heapq.heappush(heap, (0, k - 1))
+
+        while heap:
+            d, cur = heapq.heappop(heap)
+            if dist[cur] < d:
+                continue
+            
+            for nei, c in graph[cur]:
+                if dist[cur] + c < dist[nei]:
+                    dist[nei] = dist[cur] + c
+                    heapq.heappush(heap, (dist[nei], nei))  # add the updated distance with the node to heap
+        
+        
+        return max(dist) if max(dist) != math.inf else -1
+
+
+## 
