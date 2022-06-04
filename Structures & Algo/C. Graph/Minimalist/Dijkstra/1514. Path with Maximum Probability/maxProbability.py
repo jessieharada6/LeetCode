f39a72c3712f4ac54c681493e6d97f89df97d265
jessieharada6 <jessie.harada@yahoo.com>
2022-06-04 16:02:08 +1000

    reorg

--- Structures & Algo/C. Graph/Minimalist/Dijkstra/1514. Path with Maximum Probability/maxProbability.py
+++ Structures & Algo/C. Graph/Minimalist/Dijkstra/1514. Path with Maximum Probability/maxProbability.py
@@ -7,7 +7,7 @@ class Solution:
         # print(graph)
         
         heap = []
-        heapq.heappush(heap, (0, start))
+        heapq.heappush(heap, (1, start))
         
         dist = [-math.inf for _ in range(n)]                # getting max so fill in -math.inf
         dist[start] = 1                                     # note: the possibility from start to start is 1
@@ -19,6 +19,6 @@ class Solution:
                 # print(cur, nei, d)
                 if dist[cur] * d > dist[nei]:               # get max 
                     dist[nei] = dist[cur] * d
-                    heapq.heappush(heap, (-dist[nei], nei)) # turn min heap to max heap
+                    heapq.heappush(heap, (-dist[nei], nei)) # turn min heap to max heap but dist doesn't change
         # print(dist)
         return dist[end] if dist[end] != -math.inf else 0        
