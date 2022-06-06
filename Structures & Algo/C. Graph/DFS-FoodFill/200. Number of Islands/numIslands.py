859f0c52359d0195fa0383c82ccaca01f2180765
jessieharada6 <jessie.harada@yahoo.com>
2022-06-06 19:44:45 +1000

    food fill and with visited version

--- Structures & Algo/C. Graph/DFS-FoodFill/200. Number of Islands/numIslands.py
+++ Structures & Algo/C. Graph/DFS-FoodFill/200. Number of Islands/numIslands.py
@@ -1,33 +1,4 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
-        m = len(grid)
-        n = len(grid[0])
-        
-        def traverse(i, j):
-            nonlocal m
-            nonlocal n
-            if i < 0 or j < 0 or i >= m or j >= n:
-                return
-            if grid[i][j] == "0":
-                return
-            
-            grid[i][j] = "0"
-            # print(i, j)           # pre order
-            traverse(i + 1, j)      # down
-            traverse(i - 1, j)      # up
-            traverse(i, j + 1)      # right
-            traverse(i, j - 1)      # left
-            
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    islands += 1
-                    traverse(i, j)       # water all adjencent lands, avoid repetition
-        
-        return islands
-
+# foodfill
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         islands = 0
@@ -43,6 +14,42 @@ class Solution:
             
             grid[i][j] = "0"            # mark as visited
             
+            # print(i, j)               # pre order
+            traverse(i + 1, j)          # down
+            traverse(i - 1, j)          # up
+            traverse(i, j + 1)          # right
+            traverse(i, j - 1)          # left
+            
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    # print("outer", i, j)
+                    islands += 1
+                    traverse(i, j)          # water all adjencent lands, avoid repetition
+        
+        return islands
+
+
+# visited version
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        m = len(grid)
+        n = len(grid[0])
+        visited = [[False for _ in range(n)] for _ in range(m)]
+        
+        def traverse(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return
+            
+            if grid[i][j] == "0" or visited[i][j]:       # if visted, return
+                # visited[i][j] = True
+                return
+            
+            # grid[i][j] = "0"                           # mark as visited
+            visited[i][j] = True
+            
             # print(i, j)
             traverse(i + 1, j)
             traverse(i - 1, j)
@@ -52,7 +59,8 @@ class Solution:
         
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1":
+                # print(visited)
+                if not visited[i][j] and grid[i][j] == "1":
                     # print("outer", i, j)
                     islands += 1
                     traverse(i, j)
