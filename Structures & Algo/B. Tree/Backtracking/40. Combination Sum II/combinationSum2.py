2ddb1771d8ec41fa8d4b1b610397a134b45a0b62
jessieharada6 <jessie.harada@yahoo.com>
2022-06-13 17:51:30 +1000

    some notes for backtracking

--- Structures & Algo/B. Tree/Backtracking/40. Combination Sum II/combinationSum2.py
+++ Structures & Algo/B. Tree/Backtracking/40. Combination Sum II/combinationSum2.py
@@ -2,7 +2,7 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         paths = []
         n = len(candidates)
-        candidates.sort()
+        candidates.sort()           ### note
         
         def traverse(path, index, target):
             # print(path, target, index)
@@ -13,14 +13,16 @@ class Solution:
                 paths.append(path[:])
                 return
             
-            for i in range(index, n):   ### note
+            for i in range(index, n):   ### note                            ### traverse left to right
                 # i > index to ensure different elements, not i == index
                 # starting from i and i - 1, subtree is the same
-                if i > index and candidates[i] == candidates[i - 1]:
+                if i > index and candidates[i] == candidates[i - 1]:        # cut repeating tree layer (traversed before)
+                    # print(i, index)
                     continue
                 path.append(candidates[i])
+                # print(path, i, index)
                 # Each number in candidates may only be used once in the combination.
-                traverse(path, i + 1, target - candidates[i])
+                traverse(path, i + 1, target - candidates[i])               # ### traverse top down
                 path.pop()
         
         traverse([], 0, target)
