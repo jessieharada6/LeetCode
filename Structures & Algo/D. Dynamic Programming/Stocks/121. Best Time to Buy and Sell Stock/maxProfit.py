dfcc8840c5307da1e1b8d257e03195616935a0db
jessieharada6 <jessie.harada@yahoo.com>
2022-07-11 20:02:53 +1000

    121. Best Time to Buy and Sell Stock

---
+++ Structures & Algo/D. Dynamic Programming/Stocks/121. Best Time to Buy and Sell Stock/maxProfit.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        
+        dp_i_0 = 0
+        dp_i_1 = -math.inf
+        
+        for i in range(n):
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
+            dp_i_1 = max(dp_i_1, -prices[i])
+        
+        return dp_i_0
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        
+        dp = [[0] * 2 for _ in range(n)]
+        
+        print(dp)
+        for i in range(n):
+            if i - 1 == -1:
+                dp[i][0] = 0
+                dp[i][1] = -prices[i] # dp[-1][1] = -math.inf, dp[-1][0] - prices[i] = -prices[i]
+                continue
+            
+            print(i, i - 1)
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+            dp[i][1] = max(dp[i - 1][1], -prices[i])
+        
+        return dp[n - 1][0]
