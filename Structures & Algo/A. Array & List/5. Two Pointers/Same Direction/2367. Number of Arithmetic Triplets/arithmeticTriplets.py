bbcafaf830b3c617b88aaee00b9b1bd4232b8227
jessieharada6 <jessie.harada@yahoo.com>
2022-10-25 20:35:02 +1100

    pointer

--- Structures & Algo/A. Array & List/5. Two Pointers/Same Direction/2367. Number of Arithmetic Triplets/arithmeticTriplets.py
+++ Structures & Algo/A. Array & List/5. Two Pointers/Same Direction/2367. Number of Arithmetic Triplets/arithmeticTriplets.py
@@ -1,5 +1,26 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        # nums[j] - nums[i] = diff
+        # nums[k] - nums[i] = 2 * diff
+        
+        i = 0
+        j = 1
+        ans = 0
+        
+        for k in range(2, len(nums)):
+            while nums[k] - nums[j] > diff:
+                j += 1
+            if nums[k] - nums[j] != diff:
+                continue
+            while nums[j] - nums[i] > diff:
+                i += 1
+            if nums[j] - nums[i] == diff:
+                ans += 1
+        
+        return ans
+
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
         # nums[j] - nums[i] = nums[k] - nums[j] = diff
         # i < k < j
         # 以每一个当前点为j i用来看-diff k用来看+diff
