a03594d966d17e8309546f1dd2e9d73550a3105c
jessieharada6 <jessie.harada@yahoo.com>
2022-10-30 15:59:24 +1100

    sorted two sum

--- Structures & Algo/A. Array & List/5. Two Pointers/Left Right/167. Two Sum II - Input Array Is Sorted/twoSum.py
+++ Structures & Algo/A. Array & List/5. Two Pointers/Left Right/167. Two Sum II - Input Array Is Sorted/twoSum.py
@@ -1,11 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers) - 1
-        while l < r:
-            s = numbers[l] + numbers[r]
-            if s > target:
-                r -= 1
+        i = 0
+        j = len(numbers) - 1
+        
+        while i < j:
+            s = numbers[i] + numbers[j]
+            if s == target:
+                return [i + 1, j + 1]
             elif s < target:
-                l += 1
+                i += 1
             else:
-                return [l + 1, r + 1]
+                j -= 1
+                
