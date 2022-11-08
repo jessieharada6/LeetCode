ecbd447c05307a9819caf7cf397c213c7e831b73
jessieharada6 <jessie.harada@yahoo.com>
2022-11-08 18:13:38 +1100

    optimise pointers

--- Structures & Algo/A. Array & List/5. Two Pointers/Head Tail/3Sum Smaller/threeSumSmaller.py
+++ Structures & Algo/A. Array & List/5. Two Pointers/Head Tail/3Sum Smaller/threeSumSmaller.py
@@ -5,6 +5,34 @@ class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
         ans = 0
         nums.sort()
+        n = len(nums)
+        
+        for i in range(n - 2):
+            j = i + 1
+            k = n - 1
+            
+            if nums[i] + nums[i + 1] + nums[i + 2] > target:
+                break
+            
+            if nums[i] + nums[-1] + nums[-2] < target:
+                # （n - i - 1）个数中 任意选两个且不重复
+                ans += (n - i - 1) * (n - i - 2) // 2
+                continue
+            
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s < target:
+                    ans += k - j
+                    j += 1
+                else:
+                    k -= 1
+
+        return ans
+
+class Solution:
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        ans = 0
+        nums.sort()
         
         for i in range(len(nums) - 2):
             j = i + 1
