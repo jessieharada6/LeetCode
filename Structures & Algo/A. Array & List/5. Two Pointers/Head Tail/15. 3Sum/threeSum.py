686c311a4b3a64cd2ff881183f47987974e525d5
jessieharada6 <jessie.harada@yahoo.com>
2022-11-28 17:57:57 +1100

    pointers

--- Structures & Algo/A. Array & List/5. Two Pointers/Head Tail/15. 3Sum/threeSum.py
+++ Structures & Algo/A. Array & List/5. Two Pointers/Head Tail/15. 3Sum/threeSum.py
@@ -1,5 +1,39 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        ans = []
+        
+        for i in range(n - 2):
+            j = i + 1
+            k = n - 1
+            
+            if i and nums[i] == nums[i - 1]:
+                continue
+            if nums[i] + nums[i + 1] + nums[i + 2] > 0:
+                break
+            if nums[i] + nums[-1] + nums[-2] < 0:
+                continue
+            
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s < 0:
+                    j += 1
+                elif s > 0:
+                    k -= 1
+                else:
+                    ans.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+        
+        return ans
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         # i < j < k
         # i: [0 : len(nums) - 2)
         # j: i + 1
