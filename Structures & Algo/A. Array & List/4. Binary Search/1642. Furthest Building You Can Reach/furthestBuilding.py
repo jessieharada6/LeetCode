ec66f00df9f691a19f6b403c81b2ea7683783c49
jessieharada6 <jessie.harada@yahoo.com>
2022-11-28 17:57:50 +1100

    binary search

---
+++ Structures & Algo/A. Array & List/4. Binary Search/1642. Furthest Building You Can Reach/furthestBuilding.py
@@ -0,0 +1,33 @@
+class Solution: 
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        n = len(heights)
+        l, r = 0, n - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            isTrue = self.canReach(m, heights, bricks, ladders)
+            if isTrue:
+                l = m + 1
+            else:
+                r = m - 1
+        return r
+    
+    def canReach(self, m, heights, bricks, ladders):
+        diff = []
+        for i in range(m):
+            if heights[i] < heights[i + 1]:
+                diff.append(heights[i + 1] - heights[i])
+        diff.sort(reverse=True)
+        count = 0
+        for d in diff:
+            if ladders - 1 >= 0:
+                ladders -= 1
+            elif bricks - d >= 0:
+                bricks -= d
+            else:
+                break
+            count += 1
+        if count == len(diff):
+            return True
+        else:
+            return False
--- Structures & Algo/A. Array & List/4. Binary Search/1898. Maximum Number of Removable Characters/maximumRemovals.py
+++ Structures & Algo/A. Array & List/4. Binary Search/1898. Maximum Number of Removable Characters/maximumRemovals.py
@@ -1,30 +1,37 @@
 class Solution:
     def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
-        k = 0
-        sArr = list(s)
-        l, r = 0, len(removable)
-       
+        n = len(removable)
+        sList = list(s)
+        l, r = 0, n - 1
         while l <= r:
             m = (l + r) // 2
-            for i in range(m):
-                sArr[removable[i]] = 0
-            isTrue = self.isSubsequence(sArr, p)
+            sList = self.updateList(sList, removable, m)
+            isTrue = self.isSubsequence(sList, p)
             if isTrue:
                 l = m + 1
             else:
-                for i in range(l, m):
-                    sArr[removable[i]] = s[removable[i]]
+                sList = self.recoverList(s, sList, removable, m)
                 r = m - 1
-
-        return r
+        return l
     
-    def isSubsequence(self, s, p):
+    def updateList(self, sList, removable, m):
+        for i in range(m + 1):
+            sList[removable[i]] = 0
+        return sList
+    
+    def isSubsequence(self, sList, p):
         i, j = 0, 0
-        while i < len(s) and j < len(p):
-            if s[i] == p[j]:
+        while i < len(sList) and j < len(p):
+            if sList[i] == p[j]:
                 j += 1
             i += 1
-        
+
         if j == len(p):
             return True
         return False
+    
+    def recoverList(self, s, sList, removable, m):
+        for i in range(m + 1):
+            sList[removable[i]] = s[removable[i]]
+        return sList
+    
