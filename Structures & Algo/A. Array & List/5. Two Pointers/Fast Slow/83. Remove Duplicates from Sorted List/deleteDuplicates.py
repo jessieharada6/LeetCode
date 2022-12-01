8cc12cbd9b260eafa5516dec466d7d42a608c30d
jessieharada6 <jessie.harada@yahoo.com>
2022-12-01 20:37:32 +1100

    linked list

--- Structures & Algo/A. Array & List/5. Two Pointers/Fast Slow/83. Remove Duplicates from Sorted List/deleteDuplicates.py
+++ Structures & Algo/A. Array & List/5. Two Pointers/Fast Slow/83. Remove Duplicates from Sorted List/deleteDuplicates.py
@@ -1,8 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(next=head)
+        cur = head
+        pre = None
+        
+        while cur:
+            if pre and pre.val == cur.val:
+                pre.next = cur.next
+            else:
+                pre = cur
+
+            cur = cur.next
+            
+        
+        return dummy.next
+
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head: return None
--- Structures & Algo/A. Array & List/6. LinkedList/DummyHead/Merge/21. Merge Two Sorted Lists/mergeTwoLists.py
+++ Structures & Algo/A. Array & List/6. LinkedList/DummyHead/Merge/21. Merge Two Sorted Lists/mergeTwoLists.py
@@ -5,10 +5,10 @@
 #         self.next = next
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
@@ -24,6 +24,27 @@ class Solution:
             p = p.next
         
         p.next = list1 or list2
+        return dummy.next
+
+
+# Get intersection
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,4]
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = cur = ListNode(next = None)
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                list1 = list1.next
+            elif list1.val > list2.val:
+                list2 = list2.next
+            else:
+                cur.next = list1
+                cur = cur.next
+                list1 = list1.next
+                list2 = list2.next
+
         return dummy.next
 
 class Solution:
