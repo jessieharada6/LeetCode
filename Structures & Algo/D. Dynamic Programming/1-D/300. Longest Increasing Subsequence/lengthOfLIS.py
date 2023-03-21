8a74a4d1ad9be86c355e124b4847a233160eaf7e
jessieharada6 <jessie.harada@yahoo.com>
2023-03-21 19:53:20 +1100

    dp 300

--- Structures & Algo/D. Dynamic Programming/1-D/300. Longest Increasing Subsequence/lengthOfLIS.py
+++ Structures & Algo/D. Dynamic Programming/1-D/300. Longest Increasing Subsequence/lengthOfLIS.py
@@ -7,17 +7,18 @@ class Solution:
         #     mx = 1 # 每一次mx是1 因为当前数是一个子序列 结果记在return里面
         #     for j in range(i):
         #         if nums[i] > nums[j]:
-        #             mx = max(mx, dfs(j) + 1)
+        #             mx = max(mx, dfs(j) + 1)  # j本来就比i小 不用j - 1
         #     return mx
         # return max(dfs(i) for i in range(n)) # 需要提供多个入口 因为最后一个数不一定是最大的-即不一定会组成最长子序列
 
-        f = [1] * n
+        f = [0] * n
         for i, x in enumerate(nums):
             mx = 1
             for j in range(i):
                 if x > nums[j]:
                     mx = max(mx, f[j] + 1)
-                f[i] = mx
+            f[i] = mx
+        
         return max(f)
 
 # O(nlogn)
@@ -43,3 +44,23 @@ class Solution:
                 f[l] = nums[i]
 
         return len(f)            
+    
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        f = []
+
+        for x in nums:
+            l, r = 0, len(f) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if f[mid] < x:    #mid游走在f里
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            if l == len(f):
+                f.append(x)  
+            else:
+                f[l] = x
+
+        return len(f)
