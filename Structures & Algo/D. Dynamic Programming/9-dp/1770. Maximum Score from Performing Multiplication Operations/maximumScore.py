2944c388681e4f5d98fdd90045133b3d106d5a17
jessieharada6 <jessie.harada@yahoo.com>
2023-07-10 17:29:35 +1000

    dp 1770 iteration

--- Structures & Algo/D. Dynamic Programming/9-dp/1770. Maximum Score from Performing Multiplication Operations/maximumScore.py
+++ Structures & Algo/D. Dynamic Programming/9-dp/1770. Maximum Score from Performing Multiplication Operations/maximumScore.py
@@ -15,13 +15,23 @@ class Solution:
 class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
         n, m = len(nums), len(multipliers)
-        @cache
-        def dfs(l, r):
-            i = n - 1 - r + l
-            if i == m: return 0
+        # @cache
+        # def dfs(l, r):
+        #     i = n - 1 - r + l
+        #     if i == m: return 0
 
-            res1 = nums[l] * multipliers[i] + dfs(l + 1, r)
-            res2 = nums[r] * multipliers[i] + dfs(l, r - 1)
+        #     res1 = nums[l] * multipliers[i] + dfs(l + 1, r)
+        #     res2 = nums[r] * multipliers[i] + dfs(l, r - 1)
 
-            return max(res1, res2)
-        return dfs(0, n - 1)
+        #     return max(res1, res2)
+        # return dfs(0, n - 1)
+
+        f = [[0] * (n) for _ in range(m + 1)]
+        for l in range(m - 1, -1, -1): # for l in range(m - 1, 0, -1): stuck at here initially
+            for r in range(n - m, n):
+                i = n - 1 - r + l
+                if i >= m: continue
+                res1 = nums[l] * multipliers[i] + f[l + 1][r]
+                res2 = nums[r] * multipliers[i] + f[l][r - 1]
+                f[l][r] = max(res1, res2)
+        return f[0][n - 1]
