63d42457694fd5e2832a5836bb07fb6b5ad604d2
jessieharada6 <jessie.harada@yahoo.com>
2023-08-10 20:22:52 +1000

    dp

---
+++ Structures & Algo/D. Dynamic Programming/9-dp/1575. Count All Possible Routes/countRoutes.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
+        n = len(locations)
+
+        @cache
+        def dfs(cur, left): # 给定当前点和剩余fuel返回路径总数
+            res = 0
+
+            if abs(locations[cur] - locations[finish]) > left:
+                return res
+
+            if cur == finish: 
+                res += 1 # reach finish
+            
+            for i in range(n):
+                if cur != i:
+                    cost = abs(locations[cur] - locations[i])
+                    if cost <= left:
+                        res += dfs(i, left - cost) # 从定义出发 返回路径总数 +
+            
+            return res % (10 ** 9 + 7)
+        return dfs(start, fuel) 
