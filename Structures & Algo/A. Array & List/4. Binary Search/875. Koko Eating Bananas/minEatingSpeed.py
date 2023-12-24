c2027b1ab26b4c30c92a9fac07de68ec8a814bd9
jessieharada6 <jessie.harada@yahoo.com>
2023-12-24 23:16:23 +1100

    binary search koko eating banana

--- Structures & Algo/A. Array & List/4. Binary Search/875. Koko Eating Bananas/minEatingSpeed.py
+++ Structures & Algo/A. Array & List/4. Binary Search/875. Koko Eating Bananas/minEatingSpeed.py
@@ -2,6 +2,25 @@ from math import ceil
 from typing import List
 
 class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int: 
+        l, r = 0, max(piles)
+        while l + 1 < r:
+            m = (l + r) // 2
+            hours = self.getHours(piles, m)
+            print(m, hours)
+            if hours <= h:
+                r = m # r is always the correct one
+            else:
+                l = m
+        return r
+    
+    def getHours(self, piles, curBanana):
+        total = 0
+        for p in piles:
+            total += (p + curBanana - 1) // curBanana
+        return total
+
+class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         # piles.length <= h <= 10^9 -- can definitely finish all piles 
         l, r = 1, max(piles)   # bananas/hour, max(piles) -> len(piles) hours finish
