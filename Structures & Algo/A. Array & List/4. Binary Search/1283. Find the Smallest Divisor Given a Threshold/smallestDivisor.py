7ffcacd635ff3b0c851ecb4a0ff915100eaff83f
jessieharada6 <jessie.harada@yahoo.com>
2023-12-24 23:38:16 +1100

    binary search smallest divisor

--- Structures & Algo/A. Array & List/4. Binary Search/1283. Find the Smallest Divisor Given a Threshold/smallestDivisor.py
+++ Structures & Algo/A. Array & List/4. Binary Search/1283. Find the Smallest Divisor Given a Threshold/smallestDivisor.py
@@ -1,5 +1,23 @@
 class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        def getSum(k):
+            sum = 0
+            for num in nums:
+                sum += ceil(num / k)
+            return sum
+        
+        l, r = 0 , int(10e6)
+        while l + 1 < r:
+            m = (l + r) // 2
+            if getSum(m) <= threshold:
+                r = m
+            else:
+                l = m
+        
+        return r
+
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
         # k is the divisor, threshold is the result 
         
         def getSum(k): 
