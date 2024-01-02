943bcbd9058b96152ff6b2b5aadf9d332974d58c
jessieharada6 <jessie.harada@yahoo.com>
2024-01-03 09:59:18 +1100

    binary search 1011

--- Structures & Algo/A. Array & List/4. Binary Search/1011. Capacity To Ship Packages Within D Days/shipWithinDays.py
+++ Structures & Algo/A. Array & List/4. Binary Search/1011. Capacity To Ship Packages Within D Days/shipWithinDays.py
@@ -3,6 +3,33 @@ class Solution:
         def getDays(m):
             days = 1
             i = 0
+            for _ in range(len(weights)):
+                cap = m
+                while i < len(weights):
+                    if cap < weights[i]:
+                        days += 1
+                        break
+                    else:
+                        cap -= weights[i]
+                    i += 1
+                
+            return days
+
+        l, r = 0, sum(weights)
+        while l + 1 < r:
+            m = (l + r) // 2
+            if getDays(m) <= days:
+                r = m
+            else:
+                l = m
+        return r
+        
+            
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def getDays(m):
+            days = 1
+            i = 0
 
             for _ in range(len(weights)):
                 cap = m
